fb6e76782b754354180e0de77af7a7d6ff7cb139
Santiago Pintus <santiagopintus@gmail.com>
2026-08-04 14:58:55 -0300

    fix: Fixed locale selection bug

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,9 +1,12 @@
+import { setRequestLocale } from 'next-intl/server';
 import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import UniversalGameDesignFramework from '@/components/Form';
+import { Locales } from '@/i18n/request';
 
 export default async function Home({ params }: { params: Promise<{ locale: string }> }) {
-  await params;
+  const { locale } = await params;
+  setRequestLocale(locale as Locales);
 
   return (
     <div>
